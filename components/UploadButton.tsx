3dc531d7fe3c0b28d13724f184dd26e166f42fd7
unknown <mehtajinesh977@gmail.com>
2025-12-26 23:44:54 +0530

    v

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -3,14 +3,15 @@
 import { UploadButton } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
-export function ImageUpload({
-  onUploadComplete,
-}: {
+type FileUploadProps = {
   onUploadComplete: (url: string) => void;
-}) {
+  endpoint: "imageUploader" | "pdfUploader";
+};
+
+export function FileUpload({ onUploadComplete, endpoint }: FileUploadProps) {
   return (
-    <UploadButton<OurFileRouter, "imageUploader">
-      endpoint="imageUploader"
+    <UploadButton<OurFileRouter, typeof endpoint>
+      endpoint={endpoint}
       onClientUploadComplete={(res) => {
         if (res?.[0]?.url) {
           onUploadComplete(res[0].url);
